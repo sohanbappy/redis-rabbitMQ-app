94f1e038178bb7bde6c241d08c4da21ecf7ba2f8
SohanBappy <sohanbappy@gmail.com>
2021-11-06 01:25:12 +0600

    Project initialized

---
+++ src/main/java/com/bappy/cache/RedisRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.bappy.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bappy/cache/RedisRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bappy.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
